ce3cf200dfd593ef25889e781536392ffdba8c88
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2023-10-31 14:35:56 -0300

    Delete: delete frontend

--- frontend/src/components/ProtectedRoute.jsx
+++
@@ -1,7 +0,0 @@
-import { Redirect } from "react-router-dom";
-
-const ProtectedRoute = ({ children, loggedIn }) => {
-  return loggedIn ? children : <Redirect to={"/signin"} />
-}
-
-export default ProtectedRoute;
