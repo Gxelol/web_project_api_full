9152acf912266ed7d64613617bad70fa5943655d
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2023-10-31 15:59:52 -0300

    Update: update models

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const urlPattern = /(https:\/\/|http:\/\/)(w{3}\.)?[/\S]+\/?[^\S]*[#]?$/;
 const emailPattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
@@ -50,5 +51,21 @@ const userSchema = new mongoose.Schema({
     select: false,
   },
 });
+
+userSchema.statics.findUserByCredentials = (email, password) => this.findOne({ email })
+  .then((user) => {
+    if (!user) {
+      return Promise.reject(new Error('Senha ou e-mail incorreto'));
+    }
+
+    return bcrypt.compare(password, user.password)
+      .then((matched) => {
+        if (!matched) {
+          return Promise.reject(new Error('Senha ou e-mail incorreto'));
+        }
+
+        return user;
+      });
+  });
 
 module.exports = mongoose.model('user', userSchema);
