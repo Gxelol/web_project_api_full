b49a04d2f50f242bf8fd255a20141492c53afca3
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2023-10-31 16:02:41 -0300

    Update: update app

--- backend/app.js
+++ backend/app.js
@@ -32,7 +32,7 @@ app.post(
   celebrate({
     body: Joi.object().keys({
       email: Joi.string().required().email(),
-      password: Joi.string().required().min(8),
+      password: Joi.string().required().min(5).max(45),
     }),
   }),
   login,
@@ -45,8 +45,8 @@ app.post(
       name: Joi.string().min(2).max(30),
       about: Joi.string().min(2),
       avatar: Joi.string().min(2),
-      email: Joi.string().required().min(5),
-      password: Joi.string().required().min(5),
+      email: Joi.string().required().email(),
+      password: Joi.string().required().min(5).max(45),
     }),
   }),
   createUser,
@@ -62,6 +62,7 @@ app.use('*', (req, res) => {
 app.use(errorLogger);
 app.use(errors());
 
+// eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
   if (!err.statusCode && !err.message) {
     app.use((err, req, res, next) => {
